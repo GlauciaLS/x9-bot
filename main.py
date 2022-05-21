6867731a9bfe3c8074f2febfb4f5f83f44b42944
Glaucia <glaucia.ap1258@gmail.com>
2022-05-21 01:31:28 -0300

    feat: endpoint para automação de criação de PR com novos recursos

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 
-from quart import Quart, render_template, url_for
+from datetime import datetime
+from github import Github
+from quart import Quart, render_template, url_for, request
 import discord
 from discord.ext import commands
 from discord.utils import get
@@ -33,6 +35,45 @@ audio_set = {
 @app.route('/')
 async def home():
     return await render_template("index.html")
+
+
+@app.route('/file', methods=['POST'])
+async def file():
+    files = await request.files
+    file_request = files['file']
+
+    if file_request.mimetype not in ["image/gif", "audio/mpeg"]:
+        return "Tipo de arquivo inválido!"
+
+    # Token expires at 19/08/2022
+    github = Github(os.environ.get('GITHUB_TOKEN'))
+    repo = github.get_repo('GlauciaLS/x9-bot')
+    base = repo.get_branch('main')
+
+    audios = list(map(lambda audio: audio.name, repo.get_contents('resources/audio')))
+    gifs = list(map(lambda gif: gif.name, repo.get_contents('resources/gif')))
+
+    if file_request.filename in audios or file_request.filename in gifs:
+        return "no"
+
+    # Create branch
+    name_branch = f"feat/{datetime.today().strftime('%Y-%m-%d_%H-%M-%S')}"
+    new_branch = repo.create_git_ref(f"refs/heads/{name_branch}", base.commit.sha)
+
+    # Commit new file
+    new_file_content = file_request.read()
+    new_file_path = f"resources/audio/{file_request.filename}"
+
+    if file_request.mimetype == "image/gif":
+        new_file_path = f"resources/gif/{file_request.filename}"
+
+    repo.create_file(new_file_path, f'feat: adicionando {file_request.filename}', new_file_content,
+                     branch=new_branch.ref)
+
+    # Create PR
+    repo.create_pull(title=f"Adição de novo recurso: \"{file_request.filename}\"", body=f"Adição de novo recurso: \"{file_request.filename}\" através da ferramenta automatizada", head=new_branch.ref, base="main")
+
+    return "ok"
 
 
 @client.command(pass_context=True)
